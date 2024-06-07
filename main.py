fc2fbf8d44715c9c279fcf0cecfbf59302e9939a
ylmngo <aamerasim45@gmail.com>
2024-06-07 16:37:49 +0530

    Use Zip file system to write to zip files

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import re
 import shutil
 from typing import Any   
+import time
+
+from fs.memoryfs import MemoryFS
+from fs.zipfs import ZipFS
+import fs.copy 
 from dotenv import load_dotenv
 import google.generativeai as genai
 from google.ai.generativelanguage import Content, Part
@@ -24,8 +29,7 @@ class Engineer:
         prompts = read_prompts(prompt_path)        
         root_dir = os.getcwd() 
         base_dir = os.path.join(root_dir, proj_path)
-        os.makedirs(base_dir, exist_ok=True)
-        os.chdir(base_dir)
+
 
         self.begin = begin     
         self.outro = outro
@@ -58,19 +62,50 @@ class Engineer:
         return block_map
     
     def create_file_from_block(self, file_name: str, block: str): 
-        code = block 
-        
         idx = file_name.rfind('/') 
         if idx != -1: 
             dir_name = file_name[:idx]
             os.makedirs(dir_name, exist_ok=True)
         with open(file_name, "w") as f: 
-            f.write(code)
-        return 
+            f.write(block)
+
+    def create_memfile_from_block(self, file_name: str, block: str, memfs: MemoryFS): 
+        idx = file_name.rfind('/')
+        if idx != -1: 
+            dir_name = file_name[:idx]
+            memfs.makedirs(dir_name, recreate=True)
+        with memfs.open(file_name, "w") as mf: 
+            mf.write(block)
+        
+    def create_zipfile_from_block(self, file_name: str, block:str, zipfs: ZipFS): 
+        idx = file_name.rfind('/')
+        if idx != -1: 
+            dir_name = file_name[:idx]
+            zipfs.makedirs(dir_name, recreate=True)
+        with zipfs.open(file_name, "w") as zf: 
+            zf.write(block) 
 
     def create_proj(self, block_map: dict): 
         for k, v in block_map.items(): 
             self.create_file_from_block(k, v)
+
+    def create_proj_in_mem(self, block_map: dict): 
+        memfs = MemoryFS() 
+        for k, v in block_map.items(): 
+            self.create_memfile_from_block(k, v, memfs=memfs)
+        self.compress_mem_folder(memfs=memfs)
+        memfs.close() 
+
+    def create_proj_zip(self, block_map: dict): 
+        zipfs = ZipFS(f"{self.proj_name}.zip", write=True)
+        for k, v in block_map.items(): 
+            self.create_zipfile_from_block(k, v, zipfs=zipfs)
+        zipfs.close() 
+
+    def compress_mem_folder(self, memfs: MemoryFS): 
+        zipfs = ZipFS(f"{self.proj_name}.zip", write=True) 
+        fs.copy.copy_fs(memfs, zipfs)
+        zipfs.close() 
 
     def create_input(self) -> str: 
         prompts_text = ". ".join(self.prompts)
@@ -134,7 +169,7 @@ def compress_folder(zip_dir: str):
     base_name = os.path.join(root_dir, zip_dir)
     shutil.make_archive(base_name, 'zip', root_dir, zip_dir)
 
-def run(): 
+def run():
     args = parse_arguements() 
     begin = '''You will be asked to generate code for an application, your reply should include the necessary code blocks and the names of the files at the top: 
            Your reply should be structured as follows: 
@@ -145,29 +180,8 @@ def run():
     prompt = eng.create_input() 
     response = eng.generate_response(prompt)
     block_map = eng.parse_response(response=response)
-    eng.create_proj(block_map=block_map)
-    os.chdir(eng.root_dir)
-    compress_folder(eng.proj_name)
-    try: 
-        shutil.rmtree(eng.base_dir)
-    except OSError as e: 
-        print("Error: %s - %s." % (e.filename, e.strerror)) 
-        
+    eng.create_proj_zip(block_map=block_map)
+
 if __name__ == "__main__": 
     run() 
 
-
-# api_key = load_api_key(env_path)
-# os.makedirs('app/', exist_ok=True)
-
-# prompts = read_prompts(prompt_path)
-# genai.configure(api_key=api_key)
-# model = genai.GenerativeModel('gemini-pro')
-# inp = ". ".join(prompts)
-# for i in range(5): 
-#     response = model.generate_content(begin + inp+". Mention only the code blocks and the name of the file at the top of the code block")
-#     try: 
-#         block_map = parse_response(response=response.text)
-#     except ValueError: 
-#         continue 
-#     break 
